e3b5a338a356027828cc8bcc53fb21e3c840ac56
redadmiral <marco-lehner@posteo.net>
2017-12-30 16:34:47 +0100

    add functions for start and stop time tracking and filename creation

--- classes.h
+++ classes.h
@@ -3,11 +3,13 @@ using namespace std;
 
 class Ctask {
 private:
-  string m_name;
-  string m_customer;
-  float m_timespent;
+  string m_taskname;
 public:
-  Ctask (string, string);
+  Ctask (string);
   virtual ~Ctask () {};
   void getInformation ();
+  string createFilename ();
+  void createFile ();
+  void startWork ();
+  void stopWork ();
 };
