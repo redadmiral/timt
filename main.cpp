efb6757ecba609ae7444e67648b2ce09319a275e
redadmiral <marco-lehner@posteo.net>
2017-12-30 16:35:46 +0100

    routines for starting and stopping. Fundamental exception handling.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,18 @@
-#include <iostream>
 #include "classes.h"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  if (argv [1] == (string) "new") {
+      Ctask task (argv[2]);
+      task.createFile();
+  } else  if (argv [1] == (string) "start") {
+    Ctask task (argv[2]);
+    task.startWork();
+  }  else if (argv [1] == (string) "stop") {
+    Ctask task (argv[2]);
+    task.stopWork();
+  }
+  else {std::cout << "No valid input." << '\n';}
+  //std::cout << argv[1] << '\n';
+  return 0;
+}
